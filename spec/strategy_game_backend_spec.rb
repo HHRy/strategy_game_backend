ff7792316adf5dbe7a1ba887a9de09b7d32b55a0
Ryan Stenhouse <ryan@ryanstenhouse.jp>
2022-02-15 18:56:14 +0900

    Gemspec bump

--- spec/strategy_game_backend_spec.rb
+++ spec/strategy_game_backend_spec.rb
@@ -4,8 +4,4 @@ RSpec.describe StrategyGameBackend do
   it "has a version number" do
     expect(StrategyGameBackend::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
